094e2783a931b6fda43e576119a176f34590e92b
gitpeut <peut@peut.org>
2021-11-07 23:53:27 +0100

    init circular

--- src/main/java/org/peut/herdenk/security/JwtRequestFilter.java
+++ src/main/java/org/peut/herdenk/security/JwtRequestFilter.java
@@ -19,11 +19,15 @@ import java.io.IOException;
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    @Autowired
+
     private UserDetailsService userDetailsService;
+    private JwtUtil jwtUtil;
 
     @Autowired
-    private JwtUtil jwtUtil;
+    public JwtRequestFilter( UserDetailsService userDetailsService, JwtUtil jwtUtil){
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
